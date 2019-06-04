3c969d34c0df83a9ef2de5934bdb81bd67d8a788
Zaz Zhang <andrew123zhang@gmail.com>
2019-06-03 20:52:04 -0700

    last commit before code review -Andrew Zhang

--- src/exampleS3Upload.py
+++ src/exampleS3Upload.py
@@ -1,6 +1,7 @@
 import os
 
 sync_command = "aws s3 sync /home/zaz/bioda/database s3://andrew-zhang-backup-bucket --delete"
-#DOWNLOAD: sync_command = "aws s3 sync s3://andrew-zhang-backup-bucket /home/zaz/bioda/database"
-#--sync doesn't delete files, so we add --delete to properly delete files
 os.system(sync_command)
+
+#DOWNLOAD: sync_command = "aws s3 sync s3://andrew-zhang-backup-bucket /home/zaz/bioda/database"
+#note-sync doesn't delete files, so we add --delete to properly delete files
