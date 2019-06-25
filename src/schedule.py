7e8f4ba88eeef740dca4bac18ce5daaccf66f2f5
Zaz Zhang <andrew123zhang@gmail.com>
2019-06-25 15:28:49 -0700

    partitioned src files into two folders- datamanagement, jobhandling

--- src/schedule.py
+++ src/schedule.py
@@ -4,10 +4,10 @@ from crontab import CronTab
 cron = CronTab(user='zaz')
 
 # dataDownloadJob every 2 days
-dataDownloadJob = cron.new(command='python3 /home/zaz/bioda/src/Datadownload.py')
+dataDownloadJob = cron.new(command='python3 /home/zaz/bioda/src/datamanagement/Datadownload.py')
 
 # S3Interface job every 2 days
-S3InterfaceJob = cron.new(command='python3 /home/zaz/bioda/src/S3Interface.py')
+S3InterfaceJob = cron.new(command='python3 /home/zaz/bioda/src/datamanagement/S3Interface.py')
 
 #if crontab running:
 #    cron.remove_all()
